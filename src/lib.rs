6ca58772493c12e4bf38f03c2437730bf57f182e
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-08-20 19:29:25 -0400

    saving before correctness check

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
 
 pub mod algorithms;
 
-pub fn play<G: Guesser>(answer: &'static str, guesser: G) {
+pub fn play<G: Guesser>(answer: &'static str, mut guesser: G) -> Option<usize> {
     // play six round where it invokes guesser each round
+    let mut history = Vec::new();
+    // Wordle only allows six guesses.
+    // We allow more to avoid chopping off the score distribution for stats purposes.
+    for i in 1..=32 {
+        let guess = guesser.guess(&history);
+        if guess == answer {
+            return Some(i);
+        }
+        let correctness = Correctness::compute(answer, &guess);
+        history.push(Guess { 
+            word: guess, 
+            mask: correctness,
+        });
+        
+    }
+    None
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Correctness {
     // Green
     Correct,
@@ -13,6 +30,16 @@ pub enum Correctness {
     // Gray
     Wrong,
 }
+
+impl Correctness {
+    fn compute(answer: &str, guess: &str) -> [Self; 5] {
+        assert_eq!(answer.len(), 5);
+        assert_eq!(guess.len(), 5);
+        let mut c = [Correctness::Wrong; 5];
+        todo!()
+    }
+}
+
 pub struct Guess {
     pub word: String,
     pub mask: [Correctness; 5],
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ const GAMES: &str = include_str!("../answers.txt");
 fn main() {
     let guesser = atlas::algorithms::Naive::new();
     for answer in GAMES.split_whitespace() {
-        atlas::play(answer, guesser)
+        atlas::play(answer, guesser);
     }
 }
