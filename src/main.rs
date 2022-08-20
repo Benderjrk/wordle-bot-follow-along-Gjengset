094808c018d7b2173bbf52c8abc78fdf1c719881
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-08-20 14:46:13 -0400

    docs: started building frame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+const GAMES: &str = include_str!("../answers.txt");
+
 fn main() {
+    
     println!("Hello, world!");
+}
+
+fn play<G: Guesser>(answer: &'static str, guesser: G) {
+    // play six round where it invokes guesser each round
+}
+
+enum Correctness {
+    // Green
+    Correct,
+    // Yellow
+    Misplaced,
+    // Gray
+    Wrong,
+}
+struct Guess {
+    word: String,
+    mask: [Correctness; 5],
+}
+
+trait Guesser {
+    fn guess(&mut self, history: &[Guess]) -> String;
 }
